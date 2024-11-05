86702e266ff5b2d81fea8c52a1536dbee01db6b4
SHAMIL-VAKKAYIL <muhammedshamil470@gmail.com>
2024-11-05 15:01:23 +0530

    setup configuration

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Home from './pages/Home'
+import Project from './pages/Project'
+
+function App() {
+
+  return (
+    <>
+    
+      <Home />
+      <Project />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/mainComp/Hero.tsx
@@ -0,0 +1,10 @@
+
+function Hero() {
+  return (
+    <div>
+      <h3 className="text-red-500 font-extrabold">Hello</h3>
+    </div>
+  )
+}
+
+export default Hero
