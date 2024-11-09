ea9aba4ee78c6a8c9647e2552ec639f4d35ec75c
SHAMIL-VAKKAYIL <muhammedshamil470@gmail.com>
2024-11-09 20:42:37 +0530

    update: complete the project section

--- src/components/mainComp/ProjectComp.tsx
+++ src/components/mainComp/ProjectComp.tsx
@@ -1,7 +1,40 @@
 import { Link } from "react-router-dom"
 import ProjectCardComp from "../normalComp/ProjectCardComp"
+import Project from "../../pages/Project"
 
 function ProjectComp() {
+
+  const Projects = [
+    {
+      id: 1,
+      title: 'Game Explore',
+      desc: 'The Game Store App is an interactive web application designed to showcase a variety of video games, allowing users to explore games based on different genres, popularity. By integrating the RAWG API, the app pulls real-time data on the latest and trending games, giving users up-to-date information. The app features intuitive navigation, and a wishlist feature',
+      image: 'https://img.freepik.com/free-photo/view-3d-video-game-controller_23-2151005792.jpg?t=st=1731032470~exp=1731036070~hmac=e8e8e48bfb2b89b1dc9bdb296320075786a0c1a6f8fa1e1d04dba6a9d5b018f9&w=900',
+      Techs: ['React', 'TypeScript', 'Redux Toolkit', 'Tailwind'],
+      path:'https://github.com/SHAMIL-VAKKAYIL/game_store'
+
+    },
+    {
+      id: 2,
+      title: 'Global Recipe Finder',
+      desc: 'The Food Recipe Web App is a culinary exploration tool that provides users with various recipes from around the world. Using the MealDB API, the app fetches detailed recipes, including ingredients, instructions, and images, helping users discover and try new dishes.  making meal planning and cooking easier and more exciting',
+      image: 'https://img.freepik.com/free-photo/top-view-tasty-meat-soup-with-potatoes-seasonings-dark-desk_140725-76947.jpg?t=st=1731032564~exp=1731036164~hmac=a9e25f984aa75bfbcd1434c7f96417c7026df3f66fc75e786c112ec883684eba&w=900',
+      Techs: ['React', 'JavaScript', 'Redux'],
+      path:'https://github.com/SHAMIL-VAKKAYIL/food_recipe-web'
+
+    },
+    {
+      id: 3,
+      title: 'Memories Vault',
+      desc: 'This mobile app allows users to capture and preserve their precious memories by uploading videos with descriptions. Users can upload personal videos that capture special moments and add descriptive text to remember the context or story behind each video. This app is designed to provide a nostalgic experience, allowing users to relive their cherished memories anytime.',
+      image: 'https://img.freepik.com/free-photo/handheld-device-captures-stunning-city-skyline-night-generated-by-ai_188544-20370.jpg?ga=GA1.1.1144130175.1731032409&semt=ais_hybrid',
+      Techs: ['ReactNative', 'Expo', 'TypeScript', 'Appwrite', 'Redux', 'Tailwind'],
+      path:'https://github.com/SHAMIL-VAKKAYIL/aoraa_react_native'
+
+    }
+  ]
+
+
   return (
     <div className=" mx-auto sm:w-[85%] lg:w-[70%] flex flex-col   gap-y-2">
       <div className="border-2 border-secondary p-3 mt-3 rounded-lg">
@@ -9,10 +42,23 @@ function ProjectComp() {
           <h3 className='font-bld text-white text-xl bg-transparent'>Projects</h3>
         </div>
         <div className="mt-5 flex flex-col gap-7">
-          {/* <ProjectCardComp />
-            <ProjectCardComp />
-            <ProjectCardComp />
-            <ProjectCardComp /> */}
+        {Projects.map((project) => {
+          return (
+            <div key={project.id} className="mt-5 flex flex-col gap-7">
+              <ProjectCardComp 
+                description={project.desc}
+                path={project.path}
+                image={project.image}
+                imageStyle="md:h-[40vh]  2xl:h-[32vh]"
+                Techs={project.Techs}
+                title={project.title}
+                gridstyle="grid lg:grid-cols-2   2xl:h-[32vh]  "
+              />
+
+            </div>
+          )
+        })
+        }
         </div>
         <Link to='/projects'>
           <h3 className="font-bld text-white text-xl bg-transparent">More</h3>
--- src/components/normalComp/ProjectCardComp.tsx
+++ src/components/normalComp/ProjectCardComp.tsx
@@ -1,44 +1,55 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 interface IProjectCardComp {
     Techs: string[]
     image: string
     description: string
     title: string
-    descClass:string
+    descClass?: string
+    gridstyle?: string
+    imageStyle?: string
+    path: string
 }
 
-function ProjectCardComp({ Techs, title, description, image,descClass }: IProjectCardComp) {
+function ProjectCardComp({ Techs, title, description, image, descClass, gridstyle, imageStyle, path }: IProjectCardComp) {
 
-    console.log(Techs);
+
 
     return (
         <div className=''>
-            <div className='grid  gap-3 border-2  border-secondary md:border-0 rounded-lg p-3 md:p-0 overflow-hidden w-full  object-fill '>
+            <div className={`  gap-3 border-2  border-secondary md:border-0 rounded-lg p-3 md:p-0 overflow-hidden w-full  object-fill  ${gridstyle}`}>
                 <div className='w-full'>
-                    <img src={image} alt="" className=' h-[50vh] w-full  rounded-lg object-cover shadow-md shadow-gray-800' />
+                    <img src={image} alt="" className={` ${imageStyle} w-full  rounded-lg object-cover shadow-md shadow-gray-800 `} />
                 </div>
                 <div className=' text-font font-nrml flex flex-col gap-2 bg-transparent h-full  '>
-                    <div className=' flex flex-col justify-between h-full  '>
+                    <div className=' flex flex-col justify-between h-full gap-2 '>
                         <div>
-
-                            <h3 className='bg-transparent font-med text-lg'>{title}</h3>
-                            <div className=' overflow-y-scroll  md:border-2 bg-transparent border-secondary rounded-lg  p-3  '>
-                                <p className='bg-transparent md:flex hidden'>{description}</p>
-                                <p className='bg-transparent md:hidden'>{description.split('').slice(0,200).join('')} ...</p>
+                            <a href={path}>
+                                <h3 className='bg-transparent font-med md:text-xl mb-2 '>{title}</h3>
+                            </a>
+                            <div className=' scrollbar overflow-y-scroll  md:border-2 bg-transparent border-secondary rounded-lg  p-3  2xl:h-[18vh]'>
+                                <div>
+                                    <p className='bg-transparent lg:hidden xl:flex'>{description}</p>
+                                </div>
+                                <div className={`${descClass}`}>
+                                    <p className='bg-transparent lg:flex xl:hidden hidden '>{description.split('').slice(0, 200).join('')} ...</p>
+                                </div>
                             </div>
                         </div>
                         {/* Techs */}
+                        <div className=''>
 
-                        <div className='flex bg-transparent  overflow-x-scroll text-font md:gap-3 gap-1 font-med md:border-2 border-secondary rounded-lg md:py-2 md:px-2 '>
-                            {Techs.map((item) => {
-                                return (
-                                    <div key={item} className='flex  bg-transparent border-2 border-secondary rounded-xl px-2 md:px-3 py-1'>
-                                        <p className='bg-transparent'>{item}</p>
-                                    </div>
-                                )
+                            <div className='flex bg-transparent  flex-wrap 2xl:flex-nowrap  text-font md:gap-3 gap-1 font-med md:border-2 border-secondary rounded-lg md:py-2 md:px-2 2xl:overflow-x-scroll scrollbar  '>
+                                {Techs.map((item) => {
+                                    return (
+                                        <div key={item} className='flex  bg-transparent border-2 border-secondary hover:border-primary transition-transform rounded-xl px-2  py-1'>
+                                            <p className='bg-transparent'>{item}</p>
+                                        </div>
+                                    )
 
-                            })}
+                                })}
+                            </div>
                         </div>
 
 
