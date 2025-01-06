5eac1777cd65693dbac9df5c176385a9f14f655a
SHAMIL-VAKKAYIL <muhammedshamil470@gmail.com>
2025-01-06 15:13:10 +0530

    add the vercel analeytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import Project from './pages/Project'
 import { ToastContainer } from 'react-toastify'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 function App() {
 
   return (
@@ -14,9 +15,10 @@ function App() {
         <ToastContainer theme='dark' />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/projects' element={<Project />} />          
-          <Route path='/contact' element={<Contact />} />          
+          <Route path='/projects' element={<Project />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
+        <Analytics />
       </Router>
     </>
   )
